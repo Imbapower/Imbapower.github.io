e14718e1b797993e0d5792b0758a8f9ac2c68c20
Bohdan Khvorostenko <Godiegogo@ukr.net>
2018-07-04 16:54:57 +0300

    Cummit
    Cummit adaptive menu

--- go-business.kh/js/main.js
+++ go-business.kh/js/main.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
         items:1
     });
   });
+
+  
+// navbar
+(function($){
+	$(function() {
+	  $('.menu__icon').on('click', function() {
+		$(this).closest('.menu').toggleClass('menu_state_open');
+	  });
+	});
+  })(jQuery);
